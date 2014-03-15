32bc94fd49af1723f3c8a1a2aba77b60456483cd
Dimones <Dimones951@yandex.ru>
2014-03-15 19:00:43 +0700

    testcpp
    Добавлен тестовый проект Xcode, проверяю разработку консольного проекта
    C++

---
+++ IT/TestMac/testmaccpp/testmaccpp/main.cpp
@@ -0,0 +1,31 @@
+//
+//  main.cpp
+//  testmaccpp
+//
+//  Created by Дмитрий Богомолов on 15.03.14.
+//  Copyright (c) 2014 Dimones. All rights reserved.
+//
+
+#include <iostream>
+using namespace std;
+
+struct date{
+    int dd,mm,yy;
+};
+struct exam
+{
+    char name[16],profSurname[16];
+    
+};
+
+
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    //std::cout << "Hello, World!\n";
+    cout<< "Hello Mac World" << endl;
+    getchar();
+    return 0;
+}
