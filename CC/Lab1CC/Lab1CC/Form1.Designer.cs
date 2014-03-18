58ffc97756d719264834e5b34e0cfe68ce7e8778
dimones <dimka951@sibnet.ru>
2014-03-18 06:29:31 +0400

    added lab
    Lab for Computer Graphics

---
+++ CC/Lab1CC/Lab1CC/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace Lab1CC
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.glControl = new OpenTK.GLControl();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox4 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // glControl
+            // 
+            this.glControl.BackColor = System.Drawing.Color.Black;
+            this.glControl.Location = new System.Drawing.Point(12, 12);
+            this.glControl.Name = "glControl";
+            this.glControl.Size = new System.Drawing.Size(800, 600);
+            this.glControl.TabIndex = 0;
+            this.glControl.VSync = false;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(831, 116);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.Text = "X0";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(964, 116);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 3;
+            this.textBox2.Text = "Y0";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(831, 167);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 4;
+            this.textBox3.Text = "X1";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(964, 167);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(100, 20);
+            this.textBox4.TabIndex = 5;
+            this.textBox4.Text = "Y1";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(900, 75);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(113, 13);
+            this.label1.TabIndex = 9;
+            this.label1.Text = "Введите координаты";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(831, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(56, 17);
+            this.checkBox1.TabIndex = 11;
+            this.checkBox1.Text = "Сетка";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(979, 12);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(62, 17);
+            this.checkBox2.TabIndex = 12;
+            this.checkBox2.Text = "Вектор";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(831, 35);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(50, 17);
+            this.checkBox3.TabIndex = 13;
+            this.checkBox3.Text = "ЦДА";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // checkBox4
+            // 
+            this.checkBox4.AutoSize = true;
+            this.checkBox4.Location = new System.Drawing.Point(979, 35);
+            this.checkBox4.Name = "checkBox4";
+            this.checkBox4.Size = new System.Drawing.Size(82, 17);
+            this.checkBox4.TabIndex = 14;
+            this.checkBox4.Text = "Брезенхем";
+            this.checkBox4.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(831, 209);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(233, 43);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Нарисовать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1091, 627);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.checkBox4);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.glControl);
+            this.Name = "Form1";
+            this.Text = "Лаба 1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.paint);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private OpenTK.GLControl glControl;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox4;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ CC/Lab1CC/Lab1CC/Form1.cs
@@ -0,0 +1,231 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using OpenTK.Graphics.OpenGL;
+
+namespace Lab1CC
+{
+    public partial class Form1 : Form
+    {
+        int i = 0, x1 = 0, y1 = 0, x0 = 0, y0 = 0, sx = 0, sy = 0;
+        float x = 0, y = 0, dx = 0, dy = 0, l = 0;
+        bool loaded = false;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            loaded = true; GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color.Black);
+            SetupViewport();
+        }
+
+        private void SetupViewport()
+        {
+            int w = glControl.Width;
+            int h = glControl.Height;
+            GL.MatrixMode(MatrixMode.Modelview);
+            GL.LoadIdentity();
+            GL.Ortho(0, w, 0, h, -1, 1); // Верхний левый угол имеет кооординаты(0, 0)
+            GL.Viewport(0, 0, w, h); // Использовать всю поверхность GLControl под рисование
+        }
+
+
+        private void glControl1_Resize(object sender, EventArgs e)
+        {
+            if (!loaded)
+                return;
+        }
+
+        private void paint(object sender, PaintEventArgs e)
+        {
+            if (!loaded) //Пока контекст не создан
+                return;
+        }
+
+        private void btn_grid_Click(object sender, EventArgs e)
+        {
+            DrawGrid();
+        }
+
+        private void DrawGrid()
+        {
+            x0 = 0;
+            y0 = 0;
+            x = 800;
+            y = 600;
+
+            GL.LineWidth(3f);
+            GL.Color3(Color.White);
+            GL.Begin(BeginMode.Lines);
+
+            GL.Vertex2(405f, 0);
+            GL.Vertex2(405f, 800f);
+            GL.Vertex2(0, 300f);
+            GL.Vertex2(800f, 300f);
+            GL.End();
+            while (x0 <= x)
+            {
+                GL.LineWidth(1f);
+                GL.Begin(BeginMode.Lines);
+
+                GL.Color3(Color.White);
+                GL.Vertex2(x0, 0);
+                GL.Vertex2(x0, 600f);
+                x0 += 15;
+                GL.End();
+            }
+            while (y0 <= y)
+            {
+               
+                GL.Begin(BeginMode.Lines);
+                GL.LineWidth(1f);
+                GL.Color3(Color.White);
+                GL.Vertex2(0, y0);
+                GL.Vertex2(800, y0);
+                GL.End();
+                y0 += 15;
+            }
+            // glControl.SwapBuffers();
+        }
+
+        private void DrawVector()
+        {
+            try
+            {
+                x0 = Convert.ToInt32(textBox1.Text);
+                y0 = Convert.ToInt32(textBox2.Text);
+                x1 = Convert.ToInt32(textBox3.Text);
+                y1 = Convert.ToInt32(textBox4.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Проверьте заполнение полей для ввода");
+            }
+            GL.LineWidth(2f);
+            GL.Begin(BeginMode.Lines);
+            GL.Color3(Color.Red);
+            GL.Vertex2(x0 * 15 + 405, y0 * 15 + 300);
+            GL.Vertex2(x1 * 15 + 405, y1 * 15 + 300);
+            GL.End();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            glControl.SwapBuffers();
+        }
+
+        private void DrawCDA()
+        {
+            try
+            {
+                x0 = Convert.ToInt32(textBox1.Text);
+                y0 = Convert.ToInt32(textBox2.Text);
+                x1 = Convert.ToInt32(textBox3.Text);
+                y1 = Convert.ToInt32(textBox4.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Проверьте заполнение полей для ввода");
+            }
+            x = Math.Abs(x1 - x0); y = Math.Abs(y1 - y0);     //начальные координаты
+            if (x >= y) l = x; else l = y;     //определение l
+
+
+            try
+            {
+                dx = (x1 - x0) / l;
+                dy = (y1 - y0) / l; //приращения
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Длина не может быть равно 0");//ошибка при вводе в edit не числа
+                return;
+            }
+            if (dx > 0) sx = 1; else sx = -1;  //знаки приращений
+            if (dy > 0) sy = 1; else sy = -1;
+            x = Convert.ToSingle(x0 + 0.5 * sx); y = Convert.ToSingle(y0 + 0.5 * sy);
+            for (i = 0; i != l; i++)
+            { //рисуем пикселы
+
+                GL.Color3(Color.Blue);
+                GL.Rects((short)(Math.Floor(x) * 15 + 404), (short)(Math.Floor(y) * 15 + 300), 
+                    (short)(Math.Floor(x) * 15 + 420), (short)(Math.Floor(y) * 15 + 316));
+
+                x = x + dx; y = y + dy;
+            }
+        }
+
+        private void DrawBrezenhem()
+        {
+            
+            try
+            {
+                x0 = Convert.ToInt32(textBox1.Text);
+                y0 = Convert.ToInt32(textBox2.Text);
+                x1 = Convert.ToInt32(textBox3.Text);
+                y1 = Convert.ToInt32(textBox4.Text);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Проверьте заполнение полей для ввода");
+            }
+            int a = 0, b = 0, e = 0;
+
+            x = x0; y = y0;
+            dx = Math.Abs(x1 - x0); dy = Math.Abs(y1 - y0);
+            if ((x1 - x0) >= 0) sx = 1; else sx = -1;
+            if ((y1 - y0) >= 0) sy = 1; else sy = -1;
+            if (dy > dx)
+            {
+                a = Convert.ToInt32(dx); dx = dy; dy = a; b = 1;
+            }
+            else b = 0;
+            e = Convert.ToInt32(2 * dy - dx);
+            for (i = 0; i != dx; i++)
+            {
+                GL.Color3(Color.Yellow);
+                GL.Rects((short)(x * 15 + 405), (short)(y * 15 + 301), (short)(x * 15 + 421), (short)(y * 15 + 316));
+                while (e >= 0)
+                {
+                    if (b == 1) x = x + sx; else y = y + sy;
+                    e = e - Convert.ToInt32(2 * dx);
+                }
+                if (b == 1) y = y + sy; else x = x + sx;
+                e = e + Convert.ToInt32(2 * dy);
+            }
+        }
+
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.DepthBufferBit|ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color.Black);
+            if (checkBox1.Checked)
+                DrawGrid();
+            if (checkBox3.Checked)
+                DrawCDA();
+            if (checkBox4.Checked)
+                DrawBrezenhem();
+            if (checkBox2.Checked)
+                DrawVector();
+
+            glControl.SwapBuffers();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GL.Clear(ClearBufferMask.DepthBufferBit | ClearBufferMask.ColorBufferBit);
+            GL.ClearColor(Color.Black);
+            glControl.SwapBuffers();
+        }
+    }
+}
